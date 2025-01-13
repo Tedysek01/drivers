5bd3352a29844c538cfbf49766b9573e8ad92b90
Tedysek01 <tadeas@raska.eu>
2025-01-13 14:21:51 +0100

    13.1. NTB

--- lib/screens/create_event.dart
+++ lib/screens/create_event.dart
@@ -15,7 +15,12 @@ class _CreateEventPageState extends State<CreateEventPage> {
   String eventLocation = '';
   DateTime? eventDate;
   String eventType = '';
-  final List<String> eventTypes = ['Sraz', 'Závody', 'Drifty', 'Jízda okreskami'];
+  final List<String> eventTypes = [
+    'Sraz',
+    'Závody',
+    'Drifty',
+    'Jízda okreskami'
+  ];
   int? maxParticipants;
   String eventDescription = '';
   double? entryFee;
@@ -38,32 +43,81 @@ class _CreateEventPageState extends State<CreateEventPage> {
               _buildTextField(
                 label: 'Název akce',
                 onChanged: (value) => eventName = value,
-                validator: (value) => value!.isEmpty ? 'Zadejte název akce' : null,
+                validator: (value) =>
+                    value!.isEmpty ? 'Zadejte název akce' : null,
               ),
               const SizedBox(height: 20),
               // Místo konání
               _buildTextField(
                 label: 'Místo konání',
                 onChanged: (value) => eventLocation = value,
-                validator: (value) => value!.isEmpty ? 'Zadejte místo konání' : null,
+                validator: (value) =>
+                    value!.isEmpty ? 'Zadejte místo konání' : null,
               ),
               const SizedBox(height: 20),
               // Datum
-              _buildTextField(
-                label: 'Datum akce (např. 13.4.2025)',
-                onChanged: (value) {
-                  try {
-                    eventDate = DateTime.parse(value);
-                  } catch (e) {
-                    eventDate = null;
+              const SizedBox(height: 20),
+// Datum a čas
+              GestureDetector(
+                onTap: () async {
+                  DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2100),
+                  );
+
+                  if (pickedDate != null) {
+                    TimeOfDay? pickedTime = await showTimePicker(
+                      context: context,
+                      initialTime: TimeOfDay.now(),
+                    );
+
+                    if (pickedTime != null) {
+                      setState(() {
+                        eventDate = DateTime(
+                          pickedDate.year,
+                          pickedDate.month,
+                          pickedDate.day,
+                          pickedTime.hour,
+                          pickedTime.minute,
+                        );
+                      });
+                    }
                   }
                 },
-                validator: (value) => value!.isEmpty ? 'Zadejte datum akce' : null,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: colorScheme.secondary,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.all(15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        eventDate != null
+                            ? '${eventDate!.day}.${eventDate!.month}.${eventDate!.hour}:${eventDate!.minute.toString().padLeft(2, '0')}'
+                            : 'Zvolte datum a čas',
+                        style: TextStyle(
+                          color: colorScheme.onPrimary,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const Icon(
+                        Icons.calendar_today,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
               ),
+
               const SizedBox(height: 20),
               // Typ akce
               DropdownButtonFormField(
                 decoration: _inputDecoration('Typ akce'),
+                icon: Icon(Icons.arrow_drop_down, color: Colors.white,),
                 dropdownColor: colorScheme.secondary,
                 style: TextStyle(color: colorScheme.onPrimary),
                 items: eventTypes
@@ -99,44 +153,46 @@ class _CreateEventPageState extends State<CreateEventPage> {
               const SizedBox(height: 30),
               // Submit button
               ElevatedButton(
-  onPressed: () async {
-    if (_formKey.currentState!.validate()) {
-      try {
-        await FirebaseFirestore.instance.collection('events').add({
-          'name': eventName,
-          'location': eventLocation,
-          'date': eventDate?.toIso8601String(),
-          'type': eventType,
-          'description': eventDescription,
-          'maxParticipants': maxParticipants,
-          'entryFee': entryFee,
-          'createdAt': DateTime.now().toIso8601String(),
-        });
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Akce úspěšně vytvořena!')),
-        );
-        Navigator.pop(context); // Zpět na předchozí obrazovku
-      } catch (e) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Chyba při vytváření akce: $e')),
-        );
-      }
-    }
-  },
-  style: ElevatedButton.styleFrom(
-    foregroundColor: colorScheme.onPrimary,
-    backgroundColor: colorScheme.primary,
-    minimumSize: Size(200, 40),
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(10),
-    ),
-  ),
-  child: const Text(
-    'Vytvořit akci',
-    style: TextStyle(fontWeight: FontWeight.w600),
-  ),
-),
-
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    try {
+                      await FirebaseFirestore.instance
+                          .collection('events')
+                          .add({
+                        'name': eventName,
+                        'location': eventLocation,
+                        'date': eventDate?.toIso8601String(),
+                        'type': eventType,
+                        'description': eventDescription,
+                        'maxParticipants': maxParticipants,
+                        'entryFee': entryFee,
+                        'createdAt': DateTime.now().toIso8601String(),
+                      });
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content: Text('Akce úspěšně vytvořena!')),
+                      );
+                      Navigator.pop(context); // Zpět na předchozí obrazovku
+                    } catch (e) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Chyba při vytváření akce: $e')),
+                      );
+                    }
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: colorScheme.onPrimary,
+                  backgroundColor: colorScheme.primary,
+                  minimumSize: Size(200, 40),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: const Text(
+                  'Vytvořit akci',
+                  style: TextStyle(fontWeight: FontWeight.w600),
+                ),
+              ),
             ],
           ),
         ),
@@ -150,6 +206,7 @@ class _CreateEventPageState extends State<CreateEventPage> {
     TextInputType keyboardType = TextInputType.text,
     required ValueChanged<String> onChanged,
     FormFieldValidator<String>? validator,
+    
   }) {
     return TextFormField(
       decoration: _inputDecoration(label),
@@ -160,7 +217,7 @@ class _CreateEventPageState extends State<CreateEventPage> {
       keyboardType: keyboardType,
       onChanged: onChanged,
       validator: validator,
-      
+    
     );
   }
 
@@ -170,7 +227,8 @@ class _CreateEventPageState extends State<CreateEventPage> {
       labelStyle: TextStyle(color: colorScheme.onSecondary),
       border: OutlineInputBorder(
         borderRadius: BorderRadius.circular(10),
-      ),hintStyle: TextStyle(color: colorScheme.onSecondary),
+      ),
+      hintStyle: TextStyle(color: colorScheme.onSecondary),
       filled: true,
       fillColor: colorScheme.secondary,
     );
